1adb8ba266c824fadaab15f78c472d075f4c22b6
robertelving <vivalaklask@gmail.com>
2017-06-02 00:10:24 +0200

    commit test 1

--- tests/example-CHAI.test.js
+++ tests/example-CHAI.test.js
@@ -10,6 +10,7 @@ describe('application launch', function () {
     beforeEach(function () {
         if(process.platform === "darwin"){
 
+
             this.app = new Application({
                 path: './out/rct-groundcontrol-darwin-x64/rct-groundcontrol.app/Contents/MacOS/rct-groundcontrol'
             });
