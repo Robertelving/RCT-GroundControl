a4c66badb5ad085a6f50e8e17fc3e2c76a968ae2
robertelving <vivalaklask@gmail.com>
2017-06-02 00:31:39 +0200

    close #2

--- tests/example-CHAI.test.js
+++ tests/example-CHAI.test.js
@@ -9,7 +9,6 @@ chai.use(chaiAsPromised);
 describe('application launch', function () {
     beforeEach(function () {
         if(process.platform === "darwin"){
-
             this.app = new Application({
                 path: './out/rct-groundcontrol-darwin-x64/rct-groundcontrol.app/Contents/MacOS/rct-groundcontrol'
             });
